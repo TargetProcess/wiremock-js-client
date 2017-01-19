5d71c30b46c7c37c04ccf6b1fbe2ecfe8725fdca
Sergey Volkovich <volkovich@targetprocess.com>
2017-01-19 15:39:46 +0300

    Fix some namings

--- src/auth-mock-helper.ts
+++ src/auth-mock-helper.ts
@@ -29,7 +29,7 @@ export class AuthMockHelper {
         return jwt.sign({ scope: scopes }, pem, { algorithm: 'RS256', expiresIn: '1h', header: { kid: 'f3ac035dfb99d1c6f12015c014555242317159df' } })
     }
 
-    public setupJwksEndpoint() {
+    public setupMetadataEndpoint() {
         let metadata = `{"issuer":"${this.authUrl}","jwks_uri":"${this.authUrl}/.well-known/openid-configuration/jwks","authorization_endpoint":"${this.authUrl}/connect/authorize","token_endpoint":"${this.authUrl}/connect/token","userinfo_endpoint":"${this.authUrl}/connect/userinfo","end_session_endpoint":"${this.authUrl}/connect/endsession","check_session_iframe":"${this.authUrl}/connect/checksession","revocation_endpoint":"${this.authUrl}/connect/revocation","introspection_endpoint":"${this.authUrl}/connect/introspect","frontchannel_logout_supported":true,"frontchannel_logout_session_supported":true,"scopes_supported":["openid","email","tp","instances:manage","workspaces:write","workspaces:read","files:write"],"claims_supported":["sub","email","email_verified"],"response_types_supported":["code","token","id_token","id_token token","code id_token","code token","code id_token token"],"response_modes_supported":["form_post","query","fragment"],"grant_types_supported":["authorization_code","client_credentials","refresh_token","implicit"],"subject_types_supported":["public"],"id_token_signing_alg_values_supported":["RS256"],"token_endpoint_auth_methods_supported":["client_secret_basic","client_secret_post"],"code_challenge_methods_supported":["plain","S256"]}`
 
         this.wiremockClient.createJsonMock('GET', metadataEndpoint, JSON.parse(metadata))
@@ -41,7 +41,7 @@ export class AuthMockHelper {
         this.wiremockClient.createJsonMock('POST', tokenEndpoint, token_response)
     }
 
-    public setupJwksEndpointMock() {
+    public setupJwksEndpoint() {
         let jwksResponse = {
             keys: [{
                 kty: 'RSA',
